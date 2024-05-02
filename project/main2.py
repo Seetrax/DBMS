034f47170cefbf75ac2304e9337a99aa716553c3
Seetrax <vijaynaruto8@gmail.com>
2024-05-02 10:56:51 +0530

    Home page

--- project/main2.py
+++ project/main2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import psycopg2
+from streamlit.state.session_state import SessionState
 
 def authenticate(username, password):
     try:
@@ -13,27 +14,44 @@ def authenticate(username, password):
         )
 
         # If connection successful, return True
-        return True
+        st.success("Connected to PostgreSQL database successfully!")
+    except (Exception, psycopg2.Error) as error:
+        st.error("Invalid username or password. Please try again.")
+    return conn
 
-    except psycopg2.OperationalError as e:
-        # If connection fails, return False
-        return False
+def get_all_posts(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM posts")
+    posts = cursor.fetchall()
+    cursor.close()
+    return posts
 
 def main():
     st.title("DBMS Database Login")
-
+    session_state = SessionState.get(is_authenticated=False)
     # Input fields for username and password
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    
 
     # Login button
-    if st.button("Login"):
-        if authenticate(username, password):
-            st.success("Login successful!")
-            # Redirect to another page or display content after successful login
-            # For example: st.write("Welcome, {}".format(username))
-        else:
-            st.error("Invalid username or password. Please try again.")
+    if not session_state.is_authenticated:
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        if st.button("Login"):
+            conn = authenticate(username, password)
+            if conn:
+                session_state.is_authenticated = True
+
+    if session_state.is_authenticated:
+        st.header("Home")
+        conn = authenticate(username, password)
+        if conn:
+            posts = get_all_posts(conn)
+            if posts:
+                st.write("All Entries:")
+                for post in posts:
+                    st.write(post)
+            else:
+                st.write("No entries found.")
 
 if __name__ == "__main__":
     main()
